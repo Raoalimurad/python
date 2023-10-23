4093aed7766b26986326987d08476c50bfb9bdbc
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-10-23 13:44:31 +0500

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,8 @@
+#escape sequence character
+
+#\n for new line
+# print("Hello i am rao ali \n and i am a student")
+# # \ 
+# print("my name is \"rao ali murad\" and  i am a student")
+
+# print("ali",9,0 ,sep="~")

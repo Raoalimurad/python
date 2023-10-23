7c88ab5e2f87ddd3408acea2039ae87a547bfcc4
= <=raoalimurad52@gmail.com>
2023-10-23 12:32:44 +0500

    first

---
+++ 001.py
@@ -0,0 +1,65 @@
+#convert any special character into simple charcter with the help of \
+
+# print('My name is Ali and my father\'s name his "shahid pervaiz"')
+
+# name:str = "Ali Murad"
+# father_name:str = "Shahid pervaiz"
+# age:int = 19
+# card:str = "PIAIC STUDENT CARD\n"+"student name :"+name+"\nFather name :"+father_name +"\nAge is :"+ str(age)
+# print(card)
+
+
+# USE OF """ """
+# name:str = "Faizan"
+# employee:str = "software engreeing"
+# salary:int = 1000
+
+# card:str = f"""
+# employeeName : %s
+# field :%s
+# salary : %d
+# """%(name,employee,salary) #you can use this but this is complex that's why we use f string
+
+# print(card)
+
+
+
+
+
+
+
+# # NOW USING fstring
+
+
+# studentName = "rao"
+# courseName = "web 3"
+# print(f"my name is {studentName} and my course name is {courseName}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

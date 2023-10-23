938f528220edb7a7f9812d19f8dede6a369fd5e3
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-10-23 14:42:32 +0500

    Add files via upload

---
+++ 03.py
@@ -0,0 +1,11 @@
+# a = "ali"
+# b = 90
+# c = True
+# d = 9.4
+# e = None
+
+# print("the type of variable a is",type(a))
+# print("the type of variable b is",type(b))
+# print("the type of variable c is",type(c))
+# print("the type of variable d is",type(d))
+# print("the type of variable e is",type(e))

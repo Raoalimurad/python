142f2c8428e66ef1b39f5c871944a0ebb6809720
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-10-25 15:16:24 +0500

    Add files via upload

---
+++ 004.py
@@ -0,0 +1,43 @@
+# airthmetic operation
+
+a: int = 10 
+b: int = 9
+print(a + b) #addition
+print(a - b) #subtraction
+print(a * b) #multiplication
+print(a / b) #division
+print(a % b) #modules
+print(2 ** 2) #power
+print(13 // 3) #floor division
+
+
+
+#assingment operators and comparsion operator
+
+# =
+
+a: int = 10
+print(a)
+
+# ==
+a1 = 10
+b1 = "10"
+print(a1 == "10")
+
+# +=
+c = 10
+c += 2
+print(c)
+ # as well as -= , *= /=
+
+
+ # !=
+x = 9
+y = 9
+print(x != y)
+
+#< and >
+print(x > y)
+print(x < y)
+
+

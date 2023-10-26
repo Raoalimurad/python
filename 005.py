d9fb305a39a443beb2250a789cd3f70c363d367e
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-10-26 12:09:56 +0500

    Add files via upload

---
+++ 005.py
@@ -0,0 +1,15 @@
+# x: str = "abc"
+# z: str = "abc"
+
+# print(id(x))
+# print(id(z))
+# print(x is z) #is operator
+# print(x is not z) #is not operator
+
+# IN OPERATOR
+names : list[str] = ["sir zia","raoali murad","farhan"]
+userinput = input("Entre your name")
+print(userinput in names)
+
+
+
